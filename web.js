6ec2534f2c64662e8146619b3d9f9da23497df62
Ubuntu <ubuntu@ip-10-139-33-150.ec2.internal>
2013-07-14 20:38:36 +0000

    fix typo in toString

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var buf = new Buffer(fs.readFileSync('index.html'));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.ToString());
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
